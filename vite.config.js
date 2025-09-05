fba66cee150b76463c583664af8b25f82b9c4efd
Shivi Agawal <shiviagarwal253@gmail.com>
2025-09-05 17:02:59 +0530

    set base path for
    Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // ensures relative paths for assets
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
